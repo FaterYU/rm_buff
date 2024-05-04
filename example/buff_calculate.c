f857290d575b076cfe5a3af7254f0b2c2d47e638
FaterYU <1042277145@qq.com>
2024-05-04 14:35:29 +0800

    Update calculate example

--- example/buff_calculate.c
+++ example/buff_calculate.c
@@ -18,13 +18,12 @@ void calcuateBuffPostition(float xc, float yc, float zc, float theta,
 
   float r = 0.7;
 
-  float t0 = 1.0 * (cap_timestamp + t_offset) / 1000;
-  float t1 = 1.0 * self_timestamp / 1000 + delay;
+  float t0 = (float)(cap_timestamp + t_offset) / 1000;
+  float t1 = (float)(self_timestamp+ t_offset) / 1000 + delay;
   if (w == 0) {
-    theta += b * (t1 - t0) + blade_id * 2.0 / 5 * PI;
+    theta += b * (t1 - t0);
   } else {
-    theta += a / w * (cos(w * t0) - cos(w * t1)) + b * (t1 - t0) +
-             blade_id * 2.0 / 5 * PI;
+    theta += a / w * (cos(w * t0) - cos(w * t1)) + b * (t1 - t0);
   }
   *target_x = xc + r * (sin(theta) * yc / sqrt(pow(xc, 2) + pow(yc, 2)));
   *target_y = yc + r * (-sin(theta) * xc / sqrt(pow(xc, 2) + pow(yc, 2)));
